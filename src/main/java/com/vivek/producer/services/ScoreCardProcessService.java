846c4664d4e73fd84f1c2af70aaa31234b5d4362
VvePrime <vive021172@gmail.com>
2024-06-09 20:05:30 +0530

    webClient call modified for service discovery

--- src/main/java/com/vivek/producer/services/ScoreCardProcessService.java
+++ src/main/java/com/vivek/producer/services/ScoreCardProcessService.java
@@ -6,18 +6,20 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import reactor.core.publisher.Mono;
-
 @Service
 public class ScoreCardProcessService {
 
 	@Autowired
 	ScoreCardKafkaProducer scoreCardKafkaProducer;
 	
+	@Autowired
+	WebClient.Builder webClientBuilder;
+	
 	public void sendCardToProcess(String id, String scoreCard) {
 		
-		WebClient webClient = WebClient.create("http://localhost:8080");
+		WebClient webClient = webClientBuilder.build();
 		Boolean isPresent = webClient.get().uri(
+				"http://Bat-V-Ball",
 				uriBuilder -> uriBuilder
 			    .path("/api/is-player-present")
 			    .queryParam("id", id)
